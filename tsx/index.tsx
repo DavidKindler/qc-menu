18d58d3bad27e5aae4fa1a6c86396cef34991d11
DavidKindler <kindlerdavid@gmail.com>
2017-05-02 09:55:11 -0500

    install node packages

--- tsx/index.tsx
+++ tsx/index.tsx
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reducers from './reducers/index';
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware } from 'redux';
+// import reducers from './reducers/index';
 import Content from './components/content';
 
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+// const createStoreWithMiddleware = applyMiddleware()(createStore);
 interface MyProps {}
 interface MyState {}
 
